1193b48a67159499a04a0d13b2882a67f54cff19
Fahim Faisal Deepto <56530414+ffdrko@users.noreply.github.com>
2025-04-18 15:44:57 +0600

    Python print() function Done

---
+++ Python_Ground/Part-1/pg_3.py
@@ -0,0 +1,6 @@
+# Hey this line is important
+print("Hey I am a good boy\nand this viewer is also a good boy/girl.")
+print("This is a print statement.")
+print("This is a print statement.")
+print("Hey I am a \"good boy\".")
+print("Hey I am", 6, 7, sep="~", end="009\n")

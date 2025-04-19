a179380f9af215634dcbb31579e253c83c2dc30d
Fahim Faisal Deepto <56530414+ffdrko@users.noreply.github.com>
2025-04-19 13:32:00 +0600

    Python Variable, Data types Done

---
+++ Python_Ground/Part-1/pg_4.py
@@ -0,0 +1,14 @@
+a = 123
+print(a)
+a1 = 9
+b = "Harry"
+c = True
+d = None
+
+print(b)
+print(a + a1)
+
+print("The type is a is", type(a))
+print("The type is c is", type(b))
+print("The type is c is", type(c))
+print("The type is d is", type(d))

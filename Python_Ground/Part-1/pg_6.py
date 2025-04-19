48cd89123ce30f5f811264e711ec86a86446b9c0
Fahim Faisal Deepto <56530414+ffdrko@users.noreply.github.com>
2025-04-19 13:47:22 +0600

    Python Exercise-1 Done

---
+++ Python_Ground/Part-1/pg_6.py
@@ -0,0 +1,8 @@
+# Exercise 1
+a = 10
+b = 5
+
+print("The value of ", a, "+", b, "=", a + b)
+print("The value of ", a, "-", b, "=", a - b)
+print("The value of ", a, "*", b, "=", a * b)
+print("The value of ", a, "/", b, "=", a / b)

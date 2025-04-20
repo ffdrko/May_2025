6394ed0d4069fd940947e7e774c696cdb244c5aa
Fahim Faisal Deepto <56530414+ffdrko@users.noreply.github.com>
2025-04-20 12:31:16 +0600

    Python string slicing Done

---
+++ Python_Ground/Part-2/pd_2.py
@@ -0,0 +1,12 @@
+names = "Fahim,Faisal"
+print(names[0:5])
+print(names[1:4])
+print(names[:5])
+print(names[0:-3])
+print(names[-3:-1])
+
+print("names is a", len(names), "number of letters.")
+
+# quiz
+nm = "Harry"
+print(nm[-4:-2])

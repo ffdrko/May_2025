3c760bb9370a5e96e81f69822e0865d79d4fd419
Fahim Faisal Deepto <56530414+ffdrko@users.noreply.github.com>
2025-04-20 12:14:03 +0600

    Python string data type Done

---
+++ Python_Ground/Part-2/pd_1.py
@@ -0,0 +1,17 @@
+name = "Fahim"
+friend = "Faisal"
+another_friend = 'Deepto'
+apple = '''Hi Fahim, 
+hey I am good, 
+I want to eat apple.'''
+
+print("Hello," + name)
+print(apple)
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+
+for i in apple:
+    print(i)

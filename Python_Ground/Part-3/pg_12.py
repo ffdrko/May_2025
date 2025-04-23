19d4e8809f2d7b40c9ae10abd11a89e073bb15b7
Fahim Faisal Deepto <56530414+ffdrko@users.noreply.github.com>
2025-04-23 13:12:07 +0600

    Python quiz recursion Done

---
+++ Python_Ground/Part-3/pg_12.py
@@ -0,0 +1,11 @@
+def fib(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fib(n-1) + fib(n-2)
+
+
+fibo = fib(6)
+print(f"The number of fibonacci sequence is {fibo}")

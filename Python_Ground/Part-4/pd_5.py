0507a64bfa00acd9aa09c48b79d6a69d54ea6483
Fahim Faisal Deepto <56530414+ffdrko@users.noreply.github.com>
2025-04-24 12:31:42 +0600

    Python for-else loop done

---
+++ Python_Ground/Part-4/pd_5.py
@@ -0,0 +1,13 @@
+for i in range(5):
+    print(i)
+else:
+    print("end of i")
+
+fruits_lst = ["Apple", "Mango", "Cherry"]
+
+for i in fruits_lst:
+    if i == 'Banana':
+        print("Found")
+
+else:
+    print("Banana not found man!")

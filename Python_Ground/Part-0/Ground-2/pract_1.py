3dfc0a95f87fe2f2b70998e018f0a2b90ac68da9
ffdrko <faisal.deepto@gmail.com>
2025-04-28 20:07:10 +0600

    Practice ques one done

---
+++ Python_Ground/Part-0/Ground-2/pract_1.py
@@ -0,0 +1,6 @@
+# Write a program that asks the user for their name using the input() function and prints a personalized greeting.
+# Enter your name: Alice
+# Hello, Alice! Welcome to Python!
+
+user_name = input("Enter your name: ")
+print("Hello,", user_name, "! Welcome to Python!")

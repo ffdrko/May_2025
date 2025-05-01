562258c46c305cdbd0cb77daf72a79b616015df7
Fahim Faisal Deepto <56530414+ffdrko@users.noreply.github.com>
2025-05-01 13:44:20 +0600

    Python format string done

---
+++ May_2025/Python_PG/Part-2/pg_6.py
@@ -0,0 +1,6 @@
+name = 'Fahim'
+age = 55
+
+print("Hi " + name + ' You are ' + str(age) + ' years old.')
+print(f"Hi {name}, you are {age} year old.")
+print("hi {}, you are {} years old.".format(name, age))

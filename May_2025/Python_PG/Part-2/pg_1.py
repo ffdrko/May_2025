04161fbe3d6ffee333de0a4f9189256ce33e462c
Fahim Faisal Deepto <56530414+ffdrko@users.noreply.github.com>
2025-05-01 12:57:59 +0600

    Python string done

---
+++ May_2025/Python_PG/Part-2/pg_1.py
@@ -0,0 +1,11 @@
+print(type("Hi hello there 24"))
+
+user_name = 'super_coder'
+password = 'Super_Coder'
+long_string = '''
+WOW
+O O
+---
+'''
+
+print(long_string)
---
+++ May_2025/Python_PG/Part-2/pg_2.py
@@ -0,0 +1,5 @@
+first_name = 'Fahim'
+last_name = 'Deepto'
+
+full_name = first_name + ' ' + last_name
+print(full_name)

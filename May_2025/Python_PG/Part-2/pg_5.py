dcbce7e46f7935a8318a347e43182856f88abb89
Fahim Faisal Deepto <56530414+ffdrko@users.noreply.github.com>
2025-05-01 13:30:24 +0600

    Python Exercise-3 done

---
+++ May_2025/Python_PG/Part-2/pg_5.py
@@ -0,0 +1,11 @@
+print("Hello {}, your balance is {}.".format("Cindy", 50))
+# Hello cindy, your balance is 50
+
+print("Hello {0}, your balance is {1}.".format("Cindy", 50))
+# Hello cindy, your balance is 50
+
+print("Hello {name}, your balance is {amount}.".format(name="Cindy", amount=50))
+# Hello cindy, your balance is 50
+
+print("Hello {0}, your balance is {amount}.".format("Cindy", amount=50))
+# Hello cindy, your balance is 50

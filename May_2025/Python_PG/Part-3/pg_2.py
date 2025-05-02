0e634e7a0d8e945218d341ad46690d2fd1f9146a
Fahim Faisal Deepto <56530414+ffdrko@users.noreply.github.com>
2025-05-02 12:19:37 +0600

    Python Exercise-6(password checker) done.

---
+++ May_2025/Python_PG/Part-3/pg_2.py
@@ -0,0 +1,6 @@
+user_name = input("Enter your username: ")
+user_password = input("Enter your password: ")
+
+print(f"USERNAME: {user_name}")
+print(f"The user password is {"*"*len(user_password)}\n"
+      f"The length of the password is {len(user_password)}")

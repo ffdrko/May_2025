41519f937db3fbe7585d4cff7705e00b5e02fc96
Fahim Faisal Deepto <56530414+ffdrko@users.noreply.github.com>
2025-05-02 11:25:08 +0600

    Python string index done

---
+++ May_2025/Python_PG/Part-2/pg_7.py
@@ -0,0 +1,12 @@
+name = "012345"
+
+print(name[0])
+print(name[0:6])
+print(name[0:6:2])
+print(name[1:])
+print(name[:5])
+print(name[::1])
+
+print(name[-1])
+print(name[-5:-1])
+print(name[::-1])

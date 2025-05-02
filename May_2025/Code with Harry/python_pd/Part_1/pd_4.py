3c870b311aa7af46ef83707b85a5dc514f12184f
Fahim Faisal Deepto <56530414+ffdrko@users.noreply.github.com>
2025-05-02 18:13:30 +0600

    Python Escape sequence

---
+++ May_2025/Code with Harry/python_pd/Part_1/pd_4.py
@@ -0,0 +1,12 @@
+# Don't remove this line
+print("Hey I am a good boy\nand this viewer is also a good boy/girl.")
+print("\they, how are you?")
+
+print('''
+-----------------
+welcome to python
+-----------------
+''')
+
+print("It's good day, \"sunny outside\".")
+print("Hey", 669, sep="~", end="008")

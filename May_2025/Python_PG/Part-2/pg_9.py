530f49732e2fd0e9c59b37429ee3aae1ba454b75
Fahim Faisal Deepto <56530414+ffdrko@users.noreply.github.com>
2025-05-02 11:48:09 +0600

    Python str built in method done.

---
+++ May_2025/Python_PG/Part-2/pg_9.py
@@ -0,0 +1,10 @@
+greet = "helloooo"
+
+print(greet, len(greet))
+
+quote = "to be or not to be"
+
+print(quote.upper())
+print(quote.capitalize())
+print(quote.find('be'))
+print(quote.replace('be', 'been'))

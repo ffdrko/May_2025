4843942c7efadb7758e5629667adaaddc7832f2a
Fahim Faisal Deepto <56530414+ffdrko@users.noreply.github.com>
2025-05-02 12:08:38 +0600

    Python Exercise-5 done.

---
+++ May_2025/Python_PG/Part-3/pg_1.py
@@ -0,0 +1,5 @@
+user_birth_year = input('What year were you born?\n')
+current_year = 2025
+user_age = current_year - int(user_birth_year)
+
+print(f"If user born in {user_birth_year}, then now the user age is {user_age}.")

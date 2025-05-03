bed241dbd401dacbbc0f6cb39063ca7052c55d9d
Fahim Faisal Deepto <56530414+ffdrko@users.noreply.github.com>
2025-05-03 22:30:38 +0600

    Python Data type list done

---
+++ May_2025/Python_PG/Part-3/pg_3.py
@@ -0,0 +1,7 @@
+lst_1 = [1, 2, 3, 4]
+lst_2 = ['a', 'b', 'c']
+lst_3 = [1, 2.5, "a", True]
+
+print(lst_1)
+print(lst_2)
+print(lst_3)
---
+++ May_2025/Python_PG/Part-3/pg_4.py
@@ -0,0 +1,4 @@
+amazon_cart = ['notebooks', 'sunglasses']
+
+print(amazon_cart[0])
+print(amazon_cart[1])

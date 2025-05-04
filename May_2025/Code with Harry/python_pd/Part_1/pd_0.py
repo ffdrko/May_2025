4cd2727e7bece5bde8a04ba1de99e09613fdfe79
Fahim Faisal Deepto <56530414+ffdrko@users.noreply.github.com>
2025-05-04 17:07:57 +0600

    Python input function

---
+++ May_2025/Code with Harry/python_pd/Part_1/pd_0.py
@@ -0,0 +1,9 @@
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter a number: "))
+
+print("First number is", num1, "+ The Second number is", num2, "=", num1 + num2)
+print("First number is", num1, "- The Second number is", num2, "=", num1 - num2)
+print("First number is", num1, "* The Second number is", num2, "=", num1 * num2)
+print("First number is", num1, "/ The Second number is", num2, "=", num1 / num2)
+print("First number is", num1, "** The Second number is", num2, "=", num1 ** num2)
+print("First number is", num1, "% The Second number is", num2, "=", num1 % num2)
---
+++ May_2025/Code with Harry/python_pd/Part_1/pd_9.py
@@ -0,0 +1,7 @@
+user_name = input("Enter your name: ")
+print("My name is", user_name)
+
+num_1 = int(input("Enter a number: "))
+num_2 = int(input("Enter a number: "))
+print(num_1, num_2)
+print((type(num_1), type(num_2)))

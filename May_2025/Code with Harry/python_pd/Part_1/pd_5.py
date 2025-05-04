8f192d3ac55f0b40de46cc335773920dd1661103
Fahim Faisal Deepto <56530414+ffdrko@users.noreply.github.com>
2025-05-04 14:20:18 +0600

    Python Variable

---
+++ May_2025/Code with Harry/python_pd/Part_1/pd_5.py
@@ -0,0 +1,17 @@
+a = 1
+b = "Fahim"
+c = True
+d = None
+
+print(a, b, c, d)
+print("The type of a is", type(a))
+print("The type of a is", type(b))
+print("The type of a is", type(c))
+
+lst = [1, 2, 3]
+print(lst, type(lst))
+tup = (1, 2, 3)
+print(tup, type(tup))
+
+dic = {'name': "Fahim", id: 1023}
+print(dic, type(dic))

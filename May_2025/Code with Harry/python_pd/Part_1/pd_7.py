5f2cfc0aa6d69aa90ad726971d0763a5caa875b5
Fahim Faisal Deepto <56530414+ffdrko@users.noreply.github.com>
2025-05-04 16:17:47 +0600

    Python Exercise - 1

---
+++ May_2025/Code with Harry/python_pd/Part_1/pd_7.py
@@ -0,0 +1,7 @@
+num1 = 50
+num_2 = 20
+
+print("The num one is", num1, " +", "the num two is", num_2, "=", num1 + num_2)
+print("The num one is", num1, " -", "the num two is", num_2, "=", num1 - num_2)
+print("The num one is", num1, " *", "the num two is", num_2, "=", num1 * num_2)
+print("The num one is", num1, " /", "the num two is", num_2, "=", num1 / num_2)

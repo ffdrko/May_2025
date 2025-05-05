14331aafe403310ff624352c902b85e7b0798878
Fahim Faisal Deepto <56530414+ffdrko@users.noreply.github.com>
2025-05-05 11:13:13 +0600

    Python String slicing

---
+++ May_2025/Code with Harry/python_pd/Part_2/pg_1.py
@@ -0,0 +1,12 @@
+user_name = "Fahim"
+
+print(user_name[0:5])
+print(user_name[1:4])
+print(len(user_name))
+print(user_name[:5])
+print(user_name[0:-3])
+print(user_name[-3:-1])
+print(user_name[::-1])
+
+nm = "Harry"
+print(nm[-4:-2])

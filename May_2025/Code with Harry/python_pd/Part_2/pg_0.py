27b5f935b2e599b81f48195fa29d2ef6710cc7bb
Fahim Faisal Deepto <56530414+ffdrko@users.noreply.github.com>
2025-05-05 10:58:50 +0600

    Python String Data type

---
+++ May_2025/Code with Harry/python_pd/Part_2/pg_0.py
@@ -0,0 +1,17 @@
+user_name = 'Fahim'
+friend = 'Faisal'
+another_friend = 'Deepto'
+
+message_1 = "He said, \"I want to eat an apple\""
+message_2 = """
+My name is hunter,
+I do hunting
+what you do
+"""
+
+print("Hello, " + user_name)
+print(message_1)
+print(message_2)
+
+print(user_name[0])
+print(user_name[1])

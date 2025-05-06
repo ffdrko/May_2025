9e04acdb315596561684e07e2efc71aebbc4dd45
Fahim Faisal Deepto <56530414+ffdrko@users.noreply.github.com>
2025-05-06 12:12:08 +0600

    Python For loop

---
+++ May_2025/Code with Harry/python_pd/Part_3/pd_1.py
@@ -0,0 +1,16 @@
+# name = "Fahim Faisal Deepto"
+#
+# for i in name:
+#     print(i, end="")
+#
+# colors = ["Red", "Green", "Blue", "Yellow"]
+#
+# print()
+#
+# for i in colors:
+#     print(i, end="")
+#     for x in i:
+#         print(x)
+
+for i in range(1, 11):
+    print(i)

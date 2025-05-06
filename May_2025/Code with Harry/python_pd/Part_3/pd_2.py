6992c2c4979a850d4704c38c692e6de4077cd932
Fahim Faisal Deepto <56530414+ffdrko@users.noreply.github.com>
2025-05-06 12:26:27 +0600

    Python while loop

---
+++ May_2025/Code with Harry/python_pd/Part_3/pd_2.py
@@ -0,0 +1,21 @@
+# i = 1
+#
+# while i <= 5:
+#     print(i)
+#     i += 1
+#
+# x = 5
+#
+# while x >= 1:
+#     print(x)
+#     x -= 1
+
+count = 1
+
+while count<= 5:
+    print(count)
+    if count == 6:
+        print("not found")
+    count +=1
+else:
+    print('found')

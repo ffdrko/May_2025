1d2346565e76dd8448cf73e317f211298d146133
Fahim Faisal Deepto <56530414+ffdrko@users.noreply.github.com>
2025-05-06 14:33:33 +0600

    Python Function

---
+++ May_2025/Code with Harry/python_pd/Part_3/pd_4.py
@@ -0,0 +1,18 @@
+def cal_gmean(a, b):
+    mean = (a*b)/(a+b)
+    print(mean)
+
+
+def is_Greater(a, b):
+    if a > b:
+        print("First number is greater.")
+    else:
+        print("Second number is greater")
+
+
+def islesser():
+    pass
+
+
+cal_gmean(10, 20)
+is_Greater(20, 20.5)

e84fd6aef82b7a0a5d856a6ec2c2b977c0e0876f
ffdrko <faisal.deepto@gmail.com>
2025-05-19 13:31:36 +0600

    Day 1 list variable

---
+++ Jun_2025/PyGroud/Part-1/program_4.py
@@ -0,0 +1,10 @@
+text = "Enter a todo: "
+user_todo1 = input(text)
+user_todo2 = input(text)
+user_todo3 = input(text)
+
+user_todo_lst = [user_todo1, user_todo2, user_todo3, "Hello user"]
+
+print(user_todo_lst)
+print(type(user_todo_lst))
+print(type(user_todo1))

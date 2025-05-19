5f457389e170873f6639946b07c5fdee8e3c4fad
ffdrko <faisal.deepto@gmail.com>
2025-05-19 14:12:53 +0600

    Day 1 experiment done

---
+++ Jun_2025/PyGroud/Part-1/experiment/ex_2.py
@@ -0,0 +1,10 @@
+user_prompt = "Enter a todo'nt: "
+todo1 = input(user_prompt)
+todo2 = input(user_prompt)
+todo3 = input(user_prompt)
+
+todo_lst = [todo1, todo2, todo3]
+
+
+print(todo_lst)
+print(type(todo1))

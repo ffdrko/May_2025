1536635551ba6577631a7827fea25947ad385fa1
ffdrko <faisal.deepto@gmail.com>
2025-05-20 11:41:59 +0600

    Day 2 capitalize method added

--- Jun_2025/PyGroud/Part-2/program3.py
+++ Jun_2025/PyGroud/Part-2/program3.py
@@ -2,6 +2,6 @@ user_prompt = "Enter a todo: "
 todo_lst = []
 
 while True:
-    todo = input(user_prompt)
+    todo = input(user_prompt).capitalize()
     todo_lst.append(todo)
     print(todo_lst)

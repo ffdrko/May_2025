b22e13da786105364199c50ab4bd50aefd709c2a
ffdrko <faisal.deepto@gmail.com>
2025-05-20 11:53:38 +0600

    Day 2 experiment done.

---
+++ Jun_2025/PyGroud/Part-2/experiment/ex1.py
@@ -0,0 +1,2 @@
+while True:
+    print("It will run till the memory end.")
---
+++ Jun_2025/PyGroud/Part-2/experiment/ex2.py
@@ -0,0 +1,7 @@
+user_prompt = "Enter a todo: "
+todo_lst = []
+
+while True:
+    todo = input("Enter a todo: ").title()
+    todo_lst.append(todo)
+    print(todo_lst)

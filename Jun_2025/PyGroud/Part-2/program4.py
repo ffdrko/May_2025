9cc298da0c92e3a418f1c03f8a2a0434a714e7cb
ffdrko <faisal.deepto@gmail.com>
2025-05-20 12:00:19 +0600

    Day 2 password checker.

---
+++ Jun_2025/PyGroud/Part-2/program4.py
@@ -0,0 +1,7 @@
+user_pass = input("Enter your password: ")
+
+while user_pass != "pass123":
+    print("Wrong one, try again")
+    user_pass = input("Enter your password: ")
+
+print("The pass is right, welcome back man!!!")

208ddc9104016932881945e588918f37c06b988c
ffdrko <faisal.deepto@gmail.com>
2025-05-21 12:34:43 +0600

    Day 3 experiment done

---
+++ Jun_2025/PyGroud/Part-3/experiment/ex_2.py
@@ -0,0 +1,18 @@
+todo_last = []
+
+while True:
+    user_action = input("Type add, show or exit: ").strip()
+
+    match user_action:
+        case 'add':
+            user_todo = input("Enter a todo: ")
+            todo_last.append(user_todo)
+        case 'show' | 'display':
+            for item in todo_last:
+                print(item)
+        case 'exit':
+            break
+        case _:
+            print("Wrong message")
+
+print("End of operation.")

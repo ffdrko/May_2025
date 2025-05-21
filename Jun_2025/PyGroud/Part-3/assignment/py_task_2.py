3cf0a778cde22bd2db95c9c12f803ba606cf9640
ffdrko <faisal.deepto@gmail.com>
2025-05-21 12:38:57 +0600

    Day 3 assignment done.

---
+++ Jun_2025/PyGroud/Part-3/assignment/py_task_2.py
@@ -0,0 +1,9 @@
+country = "United State"
+
+match country:
+    case 'USA' | "United State":
+        print("Hello")
+    case 'Italy':
+        print("Ciao")
+    case 'Germany':
+        print('Hallo')

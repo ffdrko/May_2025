a6a0629bb8e8c5a5e7f76124544e20bcab38866a
ffdrko <faisal.deepto@gmail.com>
2025-05-21 12:22:54 +0600

    Day 3 for loop done

---
+++ Jun_2025/PyGroud/Part-3/program_3.py
@@ -0,0 +1,16 @@
+todo_last = []
+
+while True:
+    user_action = input("Type add, show or exit: ").strip()
+
+    match user_action:
+        case 'add':
+            user_todo = input("Enter a todo: ")
+            todo_last.append(user_todo)
+        case 'show':
+            for item in todo_last:
+                print(item)
+        case 'exit':
+            break
+
+print("End of operation.")

28f71f9930ffa267d617da5adb999173073bc6cf
ffdrko <faisal.deepto@gmail.com>
2025-05-25 12:55:59 +0600

    Day 5 assignment done

---
+++ Jun_2025/PyGroud/Part-5/assignment/py_task_8.py
@@ -0,0 +1,4 @@
+my_string = "Hello"
+my_list = [1, 2, 3, 4, 5]
+print(f"The length of string is {len(my_string)}")
+print(f"The length of list is {len(my_list)}")
---
+++ Jun_2025/PyGroud/Part-5/assignment/py_task_9.py
@@ -0,0 +1,4 @@
+my_lst = ['a', 'b', 'c', 'd']
+
+for i in my_lst:
+    print(f"{len(my_lst)}")

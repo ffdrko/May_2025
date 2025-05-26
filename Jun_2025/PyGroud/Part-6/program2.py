806b7ba3bcf1ba38d0f879c74a448c29833a814b
ffdrko <faisal.deepto@gmail.com>
2025-05-26 15:18:32 +0600

    Day 6 file system start.

---
+++ Jun_2025/PyGroud/Part-6/program2.py
@@ -0,0 +1,28 @@
+todo_lst = []
+
+while True:
+    user_action = input("TYpe add, show, edit, complete or exit: ").strip()
+
+    match user_action:
+        case 'add':
+            user_todo = input("Enter a todo: ") + "\n"
+            file = open('files/todo_lst.txt', 'r')
+            todo_list = file.readlines()
+            file.close()
+
+            todo_lst.append(user_todo)
+
+            file = open('files/todo_lst.txt', 'w')
+            file.writelines(todo_lst)
+            file.close()
+        case 'show':
+            for index, item in enumerate(todo_lst):
+                print(f"{index + 1}-{item}")
+        case 'edit':
+            user_todo_no = int(input("Enter the todo number to edit: "))
+            todo_lst[user_todo_no - 1] = input("Enter new todo: ")
+        case 'complete':
+            user_todo_no = int(input("Complete todo number: "))
+            todo_lst.pop(user_todo_no)
+        case 'exit':
+            break

f69835c7b41223165cd0ad4728503033fa4dd6e5
ffdrko <faisal.deepto@gmail.com>
2025-05-27 12:15:19 +0600

    Day 7 second version with list comprehension.

---
+++ Jun_2025/PyGroud/Part-7/program_3.py
@@ -0,0 +1,36 @@
+todos = []
+
+while True:
+    user_action = input("Type add, show, edit, complete or exit: ")
+
+    match user_action:
+        case 'add':
+            user_todo = input("Enter a todo: ") + "\n"
+
+            file = open('files/todo_list.txt', 'r')
+            todo_list = file.readlines()
+            file.close()
+
+            todo_list.append(user_todo)
+
+            file = open('files/todo_list.txt', 'w')
+            file.writelines(todo_list)
+            file.close()
+        case 'show':
+            file = open('files/todo_list.txt', 'r')
+            todo_list = file.readlines()
+            file.close()
+
+            new_lst = [item.strip('\n') for item in todo_list]
+
+            print(new_lst)
+            for index, item in enumerate(new_lst):
+                print(f"{index + 1}-{item}")
+        case 'edit':
+            user_todo_no = int(input("Enter the todo number to edit: "))
+            todos[user_todo_no - 1] = input("Enter a new todo: ")
+        case 'complete':
+             user_todo_no = int(input("Enter the todo number to edit: ")) - 1
+             todos.pop(user_todo_no)
+        case 'exit':
+            break

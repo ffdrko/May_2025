6e1483688ac514ec12cfee054077ae7de705eb04
ffdrko <faisal.deepto@gmail.com>
2025-05-27 13:09:27 +0600

    Day 7 bug fixed done.

---
+++ Jun_2025/PyGroud/Part-7/bug/py_bug_1.py
@@ -0,0 +1,4 @@
+temperatures = [10, 12, 14]
+
+file = open('../files/tem.txt', 'w')
+file.writelines(str(temperatures))
---
+++ Jun_2025/PyGroud/Part-7/bug/py_bug_2.py
@@ -0,0 +1,4 @@
+numbers = [10.1, 12.3, 14.7]
+numbers = [int(item) for item in numbers]
+
+print(numbers)

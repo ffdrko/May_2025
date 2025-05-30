072d3863d5c62bc85aaa13ae5e89b2f4a58869d0
ffdrko <faisal.deepto@gmail.com>
2025-05-30 22:03:07 +0600

    Day 10 experiment done.

---
+++ Jun_2025/PyGroud/Part-1/experiment/ex1.py
@@ -0,0 +1,58 @@
+while True:
+    user_action = input("Type add, show, edit, complete or exit: ").strip()
+
+    if user_action.startswith('add'):
+        user_todo = user_action[4:]
+
+        with open("../files/todo_list.txt") as file:
+            todo_list = file.readlines()
+
+        todo_list.append(user_todo + "\n")
+
+        with open("../files/todo_list.txt", "w") as file:
+            file.writelines(todo_list)
+
+    elif user_action.startswith('show'):
+        with open("../files/todo_list.txt") as file:
+            todo_list = file.readlines()
+
+        for index, item in enumerate(todo_list):
+            print(f"{index + 1}-{item.strip('\n')}")
+
+    elif user_action.startswith('edit'):
+        try:
+            todo_no = int(user_action[5:]) - 1
+
+            with open("../files/todo_list.txt") as file:
+                todo_list = file.readlines()
+
+            todo_list[todo_no] = input("Enter a new todo: ") + "\n"
+
+            with open("../files/todo_list.txt", "w") as file:
+                file.writelines(todo_list)
+        except ValueError:
+            print("../Comment statement is not right.")
+            continue
+
+    elif user_action.startswith('complete'):
+        try:
+            todo_no = int(user_action[9:]) - 1
+
+            with open("../files/todo_list.txt") as file:
+                todo_list = file.readlines()
+
+            todo_list.pop(todo_no)
+
+            with open("../files/todo_list.txt", "w") as file:
+                file.writelines(todo_list)
+        except IndexError:
+            with open("../files/todo_list.txt") as file:
+                todo_list = file.readlines()
+
+            print(f"The size of the list is {len(todo_list)}.")
+
+    elif user_action.startswith('exit'):
+        break
+
+    else:
+        print("Wrong comment!!!")

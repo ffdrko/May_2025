f1bd83b4c1b504aa32d82e4143813ea06607b669
ffdrko <faisal.deepto@gmail.com>
2025-06-01 17:57:51 +0600

    Day 11 experiment done.

---
+++ Jun_2025/PyGroud/Part-2/experiment/ex_1.py
@@ -0,0 +1,10 @@
+def greeting():
+    message = "hello"
+    new_message = message.capitalize()
+    print("hey hey")
+    # return new_message
+    print(new_message)
+
+
+text = greeting()
+print(text)

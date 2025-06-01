77d776be1ab08142627495cc543c3de96804c0cd
ffdrko <faisal.deepto@gmail.com>
2025-06-01 18:09:37 +0600

    Day 11 bonus done.

---
+++ Jun_2025/PyGroud/Part-2/program_4.py
@@ -0,0 +1,12 @@
+def get_avg():
+    with open('files/data.txt') as file:
+        local_value = file.readlines()
+
+    val = local_value[1:]
+    new_val = [float(i.strip('\n')) for i in val]
+
+    avg = sum(new_val) / len(new_val)
+    return avg
+
+average = get_avg()
+print(average)

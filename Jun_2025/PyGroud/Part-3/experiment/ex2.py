f1c2a602991855412f883cf0b73aef15fb197967
ffdrko <faisal.deepto@gmail.com>
2025-06-02 13:31:57 +0600

    Day 12 experiment done.

---
+++ Jun_2025/PyGroud/Part-3/experiment/ex2.py
@@ -0,0 +1,63 @@
+def read_todo():
+    filepath = '../files/todo_list.txt'
+    with open(filepath) as file:
+        local_list = file.readlines()
+    return local_list
+
+def write_todo():
+    filepath = '../files/todo_list.txt'
+    with open(filepath, "w") as file:
+        file.writelines(todo_list)
+
+
+while True:
+    users_action = input("Type add, show, edit, complete, exit: ").strip()
+
+    if users_action.startswith("add"):
+        user_todo = users_action[4:] + "\n"
+
+        todo_list = read_todo()
+
+        todo_list.append(user_todo)
+
+        write_todo()
+
+    elif users_action.startswith("show"):
+        todo_list = read_todo()
+
+        for index, item in enumerate(todo_list):
+            print(f"{index + 1}-{item.strip("\n")}")
+
+    elif users_action.startswith("edit"):
+        try:
+            todo_no = int(users_action[5:]) - 1
+
+            todo_list = read_todo()
+
+            todo_list[todo_no] = input("Enter new todo: ") + "\n"
+
+            write_todo()
+        except ValueError:
+            print("Please enter number after edit.")
+        except IndexError:
+            print("The number you entry is not in the list.")
+
+    elif users_action.startswith("complete"):
+        try:
+            todo_no = int(users_action[9:]) - 1
+
+            todo_list = read_todo()
+
+            todo_list.pop(todo_no)
+
+            write_todo()
+        except IndexError:
+            todo_list = read_todo()
+
+            print(f"The size of list is {len(todo_list)}")
+
+    elif users_action.startswith("exit"):
+        break
+
+    else:
+        print("Wrong input!!! please enter correct input.")

1ea75c04d03ae33bfbc0aa161fe4c7724f4ada95
ffdrko <faisal.deepto@gmail.com>
2025-06-04 14:47:15 +0600

    Day 14 bug fixed done.

---
+++ Jun_2025/PyGroud/Part-5/Bug/pybug_2.py
@@ -0,0 +1,5 @@
+import function
+
+nr_of_periods = function.count("There are good. Grass is green.")
+
+print(nr_of_periods)
---
+++ Jun_2025/PyGroud/Part-5/Bug/pybug_3.py
@@ -0,0 +1,5 @@
+from function import count
+
+nr_of_periods = count("There are good. Grass is green.")
+
+print(nr_of_periods)
